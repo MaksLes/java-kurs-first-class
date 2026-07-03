108bada10cc9ae52cb00c897bac9dfa07c29a3f1
MaksLes <maksymilianlesniak@icloud.com>
2026-07-03 15:40:08 +0200

    fix: usunięcie przypadkowo dołączonych plików GameServerStats

--- src/GameServerStats/model/PlayerKey.java
+++
@@ -1,9 +0,0 @@
-package GameServerStats.model;
-
-//Dlaczeo record idelanie nadaje się na klucz mapy?
-
-//Record jest niemutowalny wszystkie pola są final i ustawiane tylko w konstruktorze
-//kompilator automatycznie generuje poprawne hashCode() i equals() oparte na wartościach pól, a nie na adresie obiektu
-// w pamięci
-
-public record PlayerKey(String uuid, String username) {}
